3b2ff1ca43b723d7e622070e4b45814ccc470511
Victor Lyapunov <viclyapunov@users.noreply.github.com>
2025-05-14 11:29:57 +0300

    v0.01 простая форма и кнопка

---
+++ App.ps1
@@ -0,0 +1,28 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+# Создание главного окна
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Мое приложение"
+$form.Width = 300
+$form.Height = 200
+$form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
+
+# Создание кнопки
+$button = New-Object System.Windows.Forms.Button
+$button.Text = "Нажми меня"
+$button.Width = 100
+$button.Height = 30
+$button.Add_Click({
+    [System.Windows.Forms.MessageBox]::Show("Hello world", "Сообщение")
+})
+
+# Центрирование кнопки
+$button.Left = ($form.ClientSize.Width - $button.Width) / 2
+$button.Top = ($form.ClientSize.Height - $button.Height) / 2
+$button.Anchor = [System.Windows.Forms.AnchorStyles]::None
+
+# Добавление кнопки на форму
+$form.Controls.Add($button)
+
+# Показать форму
+$form.ShowDialog()
